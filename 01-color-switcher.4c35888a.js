aee8580247c29c6c5701d9414c87cb033c35a4b2
demonikos <119507967+demonikos@users.noreply.github.com>
2023-05-12 14:27:56 +0000

    Deploying to gh-pages from @ demonikos/goit-js-hw-09@b23ff56dad107584a31c7efccd025d74c369ddee 🚀

---
+++ 01-color-switcher.4c35888a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;window.addEventListener("load",(()=>{t.disabled=!1,e.disabled=!0})),t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,startInterval=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(startInterval),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4c35888a.js.map
